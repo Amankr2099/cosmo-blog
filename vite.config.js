8894e10a511321d418f7c4e5043c2c43218524d7
Amankr2099 <imamankr2121@gmail.com>
2024-03-09 20:38:46 +0530

    add chunk size spiltter

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
-// import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
+import { chunkSplitPlugin } from 'vite-plugin-chunk-split';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api':'http://localhost:3000'
-//     },
-//   },
-//   plugins: [react()],
-  
-// })
 
 export default defineConfig({
   server: {
@@ -20,11 +9,10 @@ export default defineConfig({
       '/api':'https://cosmo-blog-backend.onrender.com'
     },
   },
-  plugins: [react()],
-  // plugins: [
-  //   react(),
-  //   chunkSplitPlugin()
-  // ],
+  plugins: [
+    react(),
+    chunkSplitPlugin()
+  ],
   
   
 })
